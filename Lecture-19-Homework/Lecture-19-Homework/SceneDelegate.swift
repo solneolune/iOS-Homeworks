77937a4b2e8020b8895e229e2db5bd0b3c8b1fbd
Barbare Tepnadze <mestrolabe@Barbares-MacBook-Pro.local>
2024-04-19 23:52:22 +0400

    Upload lecture 19 homework

---
+++ Lecture-19-Homework/Lecture-19-Homework/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  Lecture-19-Homework
+//
+//  Created by Barbare Tepnadze on 19.04.24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ Lecture-19-Homework/Lecture-19-Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lecture-19-Homework
+//
+//  Created by Barbare Tepnadze on 19.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
